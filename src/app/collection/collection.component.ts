4633bca34d52b5ee4adda8581472245c418d72e3
Shlok Gandhi <shlok.gandhi@gmail.com>
2018-08-28 16:25:43 -0400

    Added Angular Service

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ibook } from '../ibook';
 import { MatSnackBar } from '@angular/material';
+import { DataService } from '../services/data.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,18 +11,12 @@ import { MatSnackBar } from '@angular/material';
 })
 export class CollectionComponent implements OnInit {
   pageTitle = 'Collection';
-  books: Array<Ibook> =
-    [
-      { id: 1, title: 'JavaScript - The Good Parts', author: 'Douglas Crockford', isCheckedOut: true, rating: 3 },
-      { id: 2, title: 'The Wind in the Willows', author: 'Kenneth Grahame', isCheckedOut: false, rating: 4 },
-      { id: 3, title: 'Pillars of the Earth', author: 'Ken Follett', isCheckedOut: true, rating: 5 },
-      { id: 4, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J. K. Rowling', isCheckedOut: false, rating: 5 }
-    ];
+  books: Array<Ibook>;
   showOperatingHours: boolean;
   openingTime: Date;
   closingTime: Date;
 
-  constructor(private _snackBar: MatSnackBar) {
+  constructor(private _snackBar: MatSnackBar, private _dataService: DataService) {
     this.openingTime = new Date();
     this.openingTime.setHours(10, 0);
     this.closingTime = new Date();
@@ -29,6 +24,7 @@ export class CollectionComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.books = this._dataService.getBooks();
   }
 
   updateMessage(message: string, type: string) {
